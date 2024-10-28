be0165a0ad153ce6dbbf6de7cbe331883e2da9e8
Sujoy De <sujoydeofficial@gmail.com>
2024-10-28 09:23:58 +0530

    add: nfthandler contract and test

--- dapp/src/NFTHandler.sol
+++ dapp/src/NFTHandler.sol
@@ -1,2 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract NFTHandler is ERC721 {
+    uint256 public currentTokenId;
+    address public owner;
+    string public baseURI;
+
+    constructor() ERC721("CryptoQuestNFT", "CQT") {
+        owner = msg.sender;
+        _mint(owner, 0);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    function mint(address to) public onlyOwner {
+        require(balanceOf(to) == 0, "ERC721: token already minted");
+        uint256 tokenId = ++currentTokenId;
+        _safeMint(to, tokenId);
+        currentTokenId += 1;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return string(abi.encodePacked(baseURI, Strings.toString(tokenId)));
+    }
+}
--- dapp/test/NFTHandler.t.sol
+++ dapp/test/NFTHandler.t.sol
@@ -1,2 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {NFTHandler} from "../src/NFTHandler.sol";
+
+contract NFTHandlerTest is Test {
+    address nftOwner = address(0x1);
+    address bob = address(0x2);
+    address alice = address(0x3);
+
+    function test_constructor() public {
+        vm.prank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        assertEq(nft.ownerOf(0), address(nftOwner));
+    }
+
+    function test_mint() public {
+        vm.startPrank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        nft.mint(bob);
+        vm.stopPrank();
+        assertEq(nft.ownerOf(1), bob);
+    }
+
+    function test_mint_fail() public {
+        vm.startPrank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        nft.mint(bob);
+        assertEq(nft.ownerOf(1), bob);
+        try nft.mint(bob) {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "ERC721: token already minted");
+        }
+        vm.stopPrank();
+        assertEq(nft.ownerOf(1), bob);
+    }
+
+    function test_mint_not_owner() public {
+        vm.prank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        vm.prank(bob);
+        try nft.mint(bob) {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "You are not the owner");
+        }
+    }
+
+    function test_setBaseURI() public {
+        vm.prank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        vm.prank(nftOwner);
+        nft.setBaseURI("https://example.com/");
+        assertEq(nft.tokenURI(0), "https://example.com/0");
+    }
+
+    function test_setBaseURI_not_owner() public {
+        vm.prank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        vm.prank(bob);
+        try nft.setBaseURI("https://example.com/") {
+            fail();
+        } catch Error(string memory reason) {
+            assertEq(reason, "You are not the owner");
+        }
+    }
+
+    function test_tokenURI() public {
+        vm.prank(nftOwner);
+        NFTHandler nft = new NFTHandler();
+        vm.prank(nftOwner);
+        nft.setBaseURI("https://example.com/");
+        assertEq(nft.tokenURI(0), "https://example.com/0");
+    }
+}
