8f6bc88c7a5c75b336e512d46a64b6a924589b11
Sujoy De <sujoydeofficial@gmail.com>
2024-11-07 08:23:16 +0530

    feat: randomize question selection and improve answer evaluation scoring

--- utils/game0.ts
+++ utils/game0.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { game0questions } from "@/db/schema";
+import { inArray, sql } from "drizzle-orm";
 
 export type Question = {
   id: string;
@@ -21,7 +22,9 @@ export const questions = await db
     option3: game0questions.option3,
     option4: game0questions.option4,
   })
-  .from(game0questions);
+  .from(game0questions)
+  .orderBy(sql`random()`)
+  .limit(10);
 
 export const getQuestions = async () => {
   const _questions = questions.map((question) => {
@@ -40,20 +43,24 @@ export const getQuestions = async () => {
 };
 
 export const evaluateAnswers = async (answers: Answer[]) => {
+  const answerIds = answers.map((answer) => answer.id);
+
   const correctAnswers = await db
     .select({
       id: game0questions.id,
       answer: game0questions.answer,
     })
-    .from(game0questions);
-  let score = 0;
-  answers.forEach((answer) => {
+    .from(game0questions)
+    .where(inArray(game0questions.id, answerIds));
+
+  const score = answers.reduce((total, answer) => {
     const correctAnswer = correctAnswers.find(
       (a) => a.id === answer.id
     )?.answer;
-    if (correctAnswer === answer.answer) {
-      score += 100 / answers.length;
-    }
-  });
+    return correctAnswer === answer.answer
+      ? total + 100 / answers.length
+      : total;
+  }, 0);
+
   return Math.floor(score);
 };
